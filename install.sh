d41d8b7ac995ab5dfb4d6b816a07bf444c5aa31b
Rafa Madolell Vargas <90782385+insanerask77@users.noreply.github.com>
2023-09-20 13:24:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-Intert into ~/.bashrc
+#!/bin/bash
 
-bind -x '"\e\e": READLINE_LINE="sudo $READLINE_LINE"; READLINE_POINT=$((READLINE_POINT + 5))'
+# Comando one-liner para insertar "sudo" al principio de la línea de comandos
+command_to_insert='bind -x '"'"'"\e\e": READLINE_LINE="sudo $READLINE_LINE"; READLINE_POINT=$((READLINE_POINT + 5))'"'">
 
-source ~/.bashrc
+# Verificar si el comando ya existe en .bashrc
+if ! grep -qF "$command_to_insert" ~/.bashrc; then
+    echo "$command_to_insert" >> ~/.bashrc
+    echo "Command inserted in en ~/.bashrc"
+else
+    echo "The command already exists in ~/.bashrc, no changes have been made."
+fi
