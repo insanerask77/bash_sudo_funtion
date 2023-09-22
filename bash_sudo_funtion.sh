df1aa1e52ef892529cc8ea14b73c51546bee4c7e
Rafa Madolell Vargas <90782385+insanerask77@users.noreply.github.com>
2023-09-22 12:55:52 +0200

    Update bash_sudo_funtion.sh

--- bash_sudo_funtion.sh
+++ bash_sudo_funtion.sh
@@ -5,6 +5,7 @@ command_to_insert='bind -x '"'"'"\e\e": READLINE_LINE="sudo $READLINE_LINE"; REA
 
 # Verificar si el comando ya existe en .bashrc
 if ! grep -qF "$command_to_insert" ~/.bashrc; then
+    cp ~/.bashrc ~/.bashrc_original
     echo "$command_to_insert" >> ~/.bashrc
     echo "Funtion installed in ~/.bashrc"
 else
