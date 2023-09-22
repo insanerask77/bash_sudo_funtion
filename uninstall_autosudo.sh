441e9ee8a5a419d32ab1c7e00c0692e8c517a99d
Rafa Madolell Vargas <90782385+insanerask77@users.noreply.github.com>
2023-09-22 12:45:29 +0200

    Create uninstall_autosudo.sh

---
+++ uninstall_autosudo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+command_to_insert='bind -x '"'"'"\e\e": READLINE_LINE="sudo $READLINE_LINE"; READLINE_POINT=$((READLINE_POINT + 5))'"'"
+
+if ! grep -qF "$command_to_insert" ~/.bashrc; then
+    echo "Funtion not installed in ~/.bashrc"
+else
+    echo "AutoSudo Uninstalled"
+    line=$(cat ~/.bashrc | grep bind)
+    cat ~/.bashrc | grep -vF "$line" > ~/.bashrc
+
+fi
